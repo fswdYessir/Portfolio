e457041e2e9a8192d8371e8dba65d002747cb317
Sol <ykim327@my.bcit.ca>
2025-03-18 21:41:34 -0700

    hero spacing change

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -14,7 +14,7 @@ function Hero() {
       <Header />
       <section
         id="hero"
-        className="relative flex flex-col md:flex-row items-center justify-center min-h-[80vh] sm:gap-15 gap-5 text-center p-10 overflow-hidden"
+        className="relative flex flex-col md:flex-row items-center justify-center min-h-[82vh] sm:gap-15 gap-5 text-center p-10 overflow-hidden"
       >
         <div className="absolute top-0 left-1/2 transform -translate-x-1/2 flex flex-col items-center z-50">
           <div className="w-1 h-12 sm:h-18 bg-gray-600 "></div>
