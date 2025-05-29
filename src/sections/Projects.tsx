d0229379812ebbbbe0e1395db70aa2c2dedbc158
Sol <ykim327@my.bcit.ca>
2025-05-29 11:01:36 +0900

    Fix project grid

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -12,7 +12,7 @@ function Projects() {
       </ScrollAnimation>
 
       <ScrollAnimation offset={100} delay={100}>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-10 justify-items-center">
+        <div className="flex flex-wrap justify-center lg:gap-8 md:gap-2 px-5 lg:max-w-[80vw] mx-auto">
           {projects.map((project, idx) => (
             <ProjectCard key={idx} {...project} />
           ))}
